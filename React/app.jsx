b040a695f5afa1bdeea00f4b01cf10f7e685e9b0
d-pamneja <dpamneja@gmail.com>
2024-10-29 21:22:55 +0530

    W9

--- React/app.jsx
+++ React/app.jsx
@@ -1,20 +1,18 @@
 import React from 'react'
 
-function App(){
+export default function App(){
     const [count,setcount] = React.useState(0)
 
-    return <Button count={count} setcount={setcount}></Button>
-}
-
-function Button(props){
     function onButtonClick(){
-        props.setcount(props.count + 1);
+       setcount(count + 1);
     }
 
-    return <button onClick={onButtonClick}>Counter ${props.count}</button>
+    return (
+        <div>
+            <button id="btn" onClick={onButtonClick}>Counter ${count}</button>
+        </div>
+    )
 }
-
-export default App
 
 
 
--- React/index.js
+++ React/index.js
@@ -1,4 +1,9 @@
-import App from "./app.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-const root = createRoot(document.getElementById("root"));
-root.render(<App/>);
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+)
