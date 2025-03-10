d8721bf73bddfc4230a7025bbcb116fe8bef7562
d-pamneja <dpamneja@gmail.com>
2025-03-10 22:46:40 +0530

    W19

--- NextJS/w18/src/app/(auth)/layout.tsx
+++ NextJS/w18/src/app/(auth)/layout.tsx
@@ -5,6 +5,7 @@ export default function AuthLayout({
   }) {
     return (
       <div className="m-auto flex h-full w-[440px] items-center justify-center px-4 md:px-0">
+        <div>Common Div for all pages in this section (auth)</div>
         {children}
       </div>
     );
